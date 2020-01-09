46e8a303025088655b1902f2f18c2ac2eaedec76
Not <JOSHUA.PONTE18@bathspa.ac.uk>
2020-01-09 13:49:56 +0000

    Basic circles implemented

--- src/Circles.cpp
+++ src/Circles.cpp
@@ -1,20 +1,27 @@
-//
-//  Circles.cpp
-//  abstractArt
-//
-//  Created by Jake Hobbs on 16/12/2019.
-//
-
 #include "Circles.hpp"
 
-void Circles::setup(){
+void Circles::setup() {
+	for ( int i = 0; i < 5 + ofRandom(10); i++ ) {
+		shape s = {
+			ofRandom( 1000 ),
+			ofRandom( 1000 ),
+			ofRandom( 100 ),
+			ofRandom( 255 ),
+			ofRandom( 255 ),
+			ofRandom( 255 )
+		};
+		this->shapes.push_back( s );
+	}
+}
+
+void Circles::update() {
 
 }
 
-void Circles::update(){
-    
-}
-
-void Circles::draw(){
-    
+void Circles::draw() {
+	for ( int i = 0; i < this->shapes.size(); i++ ) {
+		shape s = this->shapes.at( i );
+		ofSetColor( s.r , s.g , s.b );
+		ofDrawCircle( s.x , s.x , s.r );
+	}
 }
